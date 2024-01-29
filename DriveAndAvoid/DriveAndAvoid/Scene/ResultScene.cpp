4646a207dba082dff2d53b086dd082a492235262
新崎雄大 <138739177+YuutaArasaki@users.noreply.github.com>
2024-01-29 15:02:26 +0900

    ゲーム実習（縦スクロールゲーム）
    プログラムを記述した

--- DriveAndAvoid/DriveAndAvoid/Scene/ResultScene.cpp
+++ DriveAndAvoid/DriveAndAvoid/Scene/ResultScene.cpp
@@ -1,1 +1,34 @@
 #include "ResultScene.h"
+#include "../Object/RankingData.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+ResultScene::ResultScene() :back_ground(NULL), score(0)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		enemy_image[i] = NULL;
+		enemy_count[i] = NULL;
+	}
+}
+
+ResultScene::~ResultScene()
+{
+
+}
+
+//初期化処理
+void ResultScene::Initialize()
+{
+	//画像の読み込み
+	back_ground = LoadGraph("Resource/images/back.bmp");
+	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120,
+		enemy_image);
+
+	//エラーチェック
+	if (back_ground == -1)
+	{
+		throw ("Resource/images/back.bmpがありません\n");
+	}
+	//1ページ目
+}
--- DriveAndAvoid/DriveAndAvoid/Scene/ResultScene.h
+++ DriveAndAvoid/DriveAndAvoid/Scene/ResultScene.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "SceneBase.h"
+
+
 class ResultScene
 {
+private:
+	int back_ground;	//背景画像
+	int score;					//スコア
+	char name[15];				//名前
+	int enemy_count[3];			//敵のカウント
+	int enemy_image[3];			//敵画像
+
+public:
+	ResultScene();
+	virtual ~ResultScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
+
+private:
+	void ReadResultData();
 };
 
